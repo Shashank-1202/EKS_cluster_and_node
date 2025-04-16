17e12716ceeb270b22a7b38bebf94b985493c942
Shashank <shashankiyer007@gmail.com>
2025-04-16 22:17:09 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -61,13 +61,14 @@ resource "aws_eks_node_group" "main" {
   ]
 }
 
-
-
-
 # Automatically Update Kubeconfig for kubectl Access
 resource "null_resource" "update_kubeconfig_v2" {
+  triggers = {
+    cluster_name = aws_eks_cluster.main.name
+  }
+
   provisioner "local-exec" {
-    command = "aws eks --region us-east-1 update-kubeconfig --name main"
+    command = "aws eks --region ${var.region} update-kubeconfig --name ${aws_eks_cluster.main.name}"
   }
 
   depends_on = [
@@ -78,32 +79,35 @@ resource "null_resource" "update_kubeconfig_v2" {
 
 # Add Admin IAM Role to aws-auth ConfigMap
 resource "null_resource" "configure_aws_auth" {
+  triggers = {
+    cluster_name = aws_eks_cluster.main.name
+  }
+
   provisioner "local-exec" {
     command = <<EOT
-      cat <<EOF | kubectl apply -f -
-      apiVersion: v1
-      kind: ConfigMap
-      metadata:
-        name: aws-auth
-        namespace: kube-system
-      data:
-        mapRoles: |
-          - rolearn: ${data.aws_iam_role.eks_node_role.arn}
-            username: system:node:EC2PrivateDNSName
-            groups:
-              - system:bootstrappers
-              - system:nodes
-          - rolearn: ${data.aws_iam_role.eks_role.arn}
-            username: admin
-            groups:
-              - system:masters
-      EOF
-    EOT
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${data.aws_iam_role.eks_node_role.arn}
+      username: system:node:EC2PrivateDNSName
+      groups:
+        - system:bootstrappers
+        - system:nodes
+    - rolearn: ${data.aws_iam_role.eks_role.arn}
+      username: admin
+      groups:
+        - system:masters
+EOF
+EOT
   }
 
   depends_on = [
-  aws_eks_cluster.main,
-  null_resource.update_kubeconfig_v2
-]
+    aws_eks_cluster.main,
+    null_resource.update_kubeconfig_v2
+  ]
 }
-
